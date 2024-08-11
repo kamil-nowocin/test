2e209cf2488fb500dcf3f6e5995a3df7d5a0dbc9
Kamil Nowocin <knowocin@sii.pl>
2024-08-11 15:49:27 +0200

    Actions Tests

--- .github/scripts/send-slack-notification.sh
+++ .github/scripts/send-slack-notification.sh
@@ -3,14 +3,14 @@
 # Fetch arguments
 BUILD_RESULT=$1
 PR_TITLE=$2
-COMMIT_SHA=$3
+PR_USER=$3
 REPO=$4
 RUN_ID=$5
 
 # Check if the build failed
 if [ "$BUILD_RESULT" == "failure" ]; then
   curl -X POST -H 'Content-type: application/json' --data "{
-    \"text\": \"❌ The Gradle build failed!\nPR: \\\"$PR_TITLE\\\"\nCommit: $COMMIT_SHA\nURL: https://github.com/$REPO/actions/runs/$RUN_ID\",
+    \"text\": \"❌ The Gradle build failed!\nPR: \\\"$PR_TITLE\\\" by @$PR_USER\nURL: https://github.com/$REPO/actions/runs/$RUN_ID\",
     \"username\": \"GitHub Actions\",
     \"icon_emoji\": \":warning:\"
   }" $SLACK_WEBHOOK_URL
