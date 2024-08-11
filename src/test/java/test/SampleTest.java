0f6b03359e7c15f567303b4cfbd5833e41978400
Kamil Nowocin <knowocin@sii.pl>
2024-08-11 14:49:28 +0200

    Actions Tests

---
+++ src/test/java/test/SampleTest.java
@@ -0,0 +1,15 @@
+/* (C) 2022 | Kamil Nowocin */
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    private void navigateToMainPage() {
+        var a = 1;
+        var b = 2;
+        Assert.assertEquals(a, b);
+    }
+}
