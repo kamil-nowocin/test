654817d8503c0a02922d647c44d3af7f62388988
Kamil Nowocin <knowocin@sii.pl>
2024-08-11 15:17:38 +0200

    Actions Tests

--- .github/scripts/post-comment.js
+++ .github/scripts/post-comment.js
@@ -1,11 +1,13 @@
-const url = `${process.env.GITHUB_SERVER_URL}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`;
-const message = process.env.build_result === 'success'
-    ? '✅KAAAAAAAAAAAAA The Gradle build completed successfully without any issues!'
-    : `❌ kAAAAAAAAAAAAAAA The Gradle build failed!\nPlease check the details: [View Workflow Run](${url})`;
+module.exports = async function postComment(github, context) {
+  const url = `${process.env.GITHUB_SERVER_URL}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`;
+  const message = process.env.build_result === 'success'
+      ? '✅ KAAAAAAAAAAAThe Gradle build completed successfully without any issues!'
+      : `❌ KAAAAAAAAAAAAThe Gradle build failed!\nPlease check the details: [View Workflow Run](${url})`;
 
-github.rest.issues.createComment({
-  issue_number: context.issue.number,
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  body: message
-});
+  await github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: message
+  });
+};
