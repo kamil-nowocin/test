592f174d1754a1cc60d421dfb416aab147ccbbcd
Kamil Nowocin <knowocin@sii.pl>
2024-08-11 19:52:33 +0200

    Actions Tests

--- .github/scripts/post-comment.js
+++ .github/scripts/post-comment.js
@@ -1,14 +1,14 @@
 async function postComment(github, context) {
-  const { owner, repo } = context.repo;
+  const {owner, repo} = context.repo;
   const issue_number = context.issue.number;
   const run_number = context.runNumber;
   const url = `${process.env.GITHUB_SERVER_URL}/${owner}/${repo}/actions/runs/${context.runId}`;
   const message = process.env.build_result === 'success'
       ? `### Gradle build completed successfully! ✅\nPR is ready for review!`
-      : `### Gradle build failed! ❌\nPR isn't ready for review! Please check Github Actions Workflow [Run #${run_number}](${url}) for more information`;
+      : `### Gradle build failed! ❌\nPR isn't ready for review! Please check Github Actions Workflow [Run #${run_number}](${url}) for more information.`;
 
   try {
-    await github.rest.issues.createComment({ owner, repo, issue_number, body: message });
+    await github.rest.issues.createComment({owner, repo, issue_number, body: message});
   } catch (error) {
     console.error(`Error posting comment: ${error.message}`);
   }
--- .github/scripts/send-slack-notification.js
+++ .github/scripts/send-slack-notification.js
@@ -1,26 +1,23 @@
 const axios = require('axios');
 
-async function sendSlackNotification(buildResult, prTitle, prUser, repo, runId, prNumber) {
+async function sendSlackNotification(buildResult, prTitle, prUser, repo, runId,
+    prNumber) {
   if (buildResult === 'failure') {
     const workflowUrl = `https://github.com/${repo}/actions/runs/${runId}`;
     const prUrl = `https://github.com/${repo}/pull/${prNumber}`;
-    const message = `*Gradle build failed!*❌\nPR: "<${prUrl}|${prTitle}>" by @${prUser}\nWorkflow URL: <${workflowUrl}>`;
+    // const message = `*Gradle build failed!*❌\nPR: "<${prUrl}|${prTitle}>" by @${prUser}\nWorkflow URL: <${workflowUrl}>`;
+    const message = `*Gradle build failed!*❌\nPR: "<\\${prUrl}|\\${prTitle}>" by @\\${prUser}\nWorkflow URL: <\\${workflowUrl}>`;
 
     try {
       await axios.post(process.env.SLACK_WEBHOOK_URL, {
-        text: message,
-        username: 'GitHub Actions',
-        icon_emoji: ':warning:',
+        text: message, username: 'GitHub Actions', icon_emoji: ':warning:',
       });
-      console.log('Slack notification sent successfully');
+      console.log('Slack notification sent successfully!');
     } catch (error) {
       console.error('Error sending Slack notification:', error.message);
     }
   }
 }
 
-// Get command line arguments passed from the workflow
 const [buildResult, prTitle, prUser, repo, runId, prNumber] = process.argv.slice(2);
-
-// Call the function to send the Slack notification
 sendSlackNotification(buildResult, prTitle, prUser, repo, runId, prNumber);
