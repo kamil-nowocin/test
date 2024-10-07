daa6aa06bfc95b496481415e9d7ec08dfdb90d37
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 20:53:49 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -6,6 +6,9 @@ for MODULE in "${TEST_MODULES[@]}"; do
   echo "Processing TestNG results for \"$MODULE\""
   MODULE_PATH="$GITHUB_WORKSPACE/$MODULE/build/reports/tests/test"
 
+  # Display MODULE_PATH for debugging purposes
+  echo "MODULE_PATH: $MODULE_PATH"
+
   if [ -d "$MODULE_PATH" ]; then
     mkdir -p "artifacts/$MODULE"
     cp -r "$MODULE_PATH" "artifacts/$MODULE/"
