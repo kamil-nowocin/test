56865ee65075d081020987ea5ae06df72ecd6d6b
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 21:06:28 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -2,9 +2,14 @@
 
 mkdir -p artifacts
 
-for MODULE in $TEST_MODULES; do
+# Properly iterate over the elements in the array
+for MODULE in "${TEST_MODULES[@]}"; do
   echo "Processing TestNG results for \"$MODULE\""
   MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
+
+  # Display MODULE_PATH for debugging purposes
+  echo "MODULE_PATH: $MODULE_PATH"
+
   if [ -d "$MODULE_PATH" ]; then
     mkdir -p "artifacts/$MODULE"
     cp -r "$MODULE_PATH" "artifacts/$MODULE/"
@@ -12,3 +17,17 @@ for MODULE in $TEST_MODULES; do
     echo "No TestNG results found for \"$MODULE\""
   fi
 done
+
+#
+#mkdir -p artifacts
+#
+#for MODULE in $TEST_MODULES; do
+#  echo "Processing TestNG results for \"$MODULE\""
+#  MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
+#  if [ -d "$MODULE_PATH" ]; then
+#    mkdir -p "artifacts/$MODULE"
+#    cp -r "$MODULE_PATH" "artifacts/$MODULE/"
+#  else
+#    echo "No TestNG results found for \"$MODULE\""
+#  fi
+#done
