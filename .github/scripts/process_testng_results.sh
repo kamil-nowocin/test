4b7908dc8ea76cd0d77f98f08d839f0d0d357043
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 21:09:05 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -6,11 +6,11 @@ mkdir -p artifacts
 if [[ "${#TEST_MODULES[@]}" -gt 1 ]]; then
   echo "TEST_MODULES is treated as an array"
   for MODULE in "${TEST_MODULES[@]}"; do
-    echo "Processing TestNG results for \"$MODULE\""
+    echo "1Processing TestNG results for \"$MODULE\""
     MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
 
     # Display MODULE_PATH for debugging purposes
-    echo "MODULE_PATH: $MODULE_PATH"
+    echo "1MODULE_PATH: $MODULE_PATH"
 
     if [ -d "$MODULE_PATH" ]; then
       mkdir -p "artifacts/$MODULE"
@@ -23,11 +23,11 @@ else
   echo "TEST_MODULES is being treated as a single string"
   # If treated as a single string, iterate over space-separated values
   for MODULE in $TEST_MODULES; do
-    echo "Processing TestNG results for \"$MODULE\""
+    echo "2Processing TestNG results for \"$MODULE\""
     MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
 
     # Display MODULE_PATH for debugging purposes
-    echo "MODULE_PATH: $MODULE_PATH"
+    echo "2MODULE_PATH: $MODULE_PATH"
 
     if [ -d "$MODULE_PATH" ]; then
       mkdir -p "artifacts/$MODULE"
