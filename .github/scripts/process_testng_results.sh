2f7e22e07eb8b6bc9c1f5e850c8a82479f4d79d9
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 21:08:52 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -2,21 +2,42 @@
 
 mkdir -p artifacts
 
-# Properly iterate over the elements in the array
-for MODULE in "${TEST_MODULES[@]}"; do
-  echo "Processing TestNG results for \"$MODULE\""
-  MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
+# Properly iterate over elements in the array
+if [[ "${#TEST_MODULES[@]}" -gt 1 ]]; then
+  echo "TEST_MODULES is treated as an array"
+  for MODULE in "${TEST_MODULES[@]}"; do
+    echo "Processing TestNG results for \"$MODULE\""
+    MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
 
-  # Display MODULE_PATH for debugging purposes
-  echo "MODULE_PATH: $MODULE_PATH"
+    # Display MODULE_PATH for debugging purposes
+    echo "MODULE_PATH: $MODULE_PATH"
 
-  if [ -d "$MODULE_PATH" ]; then
-    mkdir -p "artifacts/$MODULE"
-    cp -r "$MODULE_PATH" "artifacts/$MODULE/"
-  else
-    echo "No TestNG results found for \"$MODULE\""
-  fi
-done
+    if [ -d "$MODULE_PATH" ]; then
+      mkdir -p "artifacts/$MODULE"
+      cp -r "$MODULE_PATH" "artifacts/$MODULE/"
+    else
+      echo "No TestNG results found for \"$MODULE\""
+    fi
+  done
+else
+  echo "TEST_MODULES is being treated as a single string"
+  # If treated as a single string, iterate over space-separated values
+  for MODULE in $TEST_MODULES; do
+    echo "Processing TestNG results for \"$MODULE\""
+    MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
+
+    # Display MODULE_PATH for debugging purposes
+    echo "MODULE_PATH: $MODULE_PATH"
+
+    if [ -d "$MODULE_PATH" ]; then
+      mkdir -p "artifacts/$MODULE"
+      cp -r "$MODULE_PATH" "artifacts/$MODULE/"
+    else
+      echo "No TestNG results found for \"$MODULE\""
+    fi
+  done
+fi
+
 
 #
 #mkdir -p artifacts
