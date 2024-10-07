5e7b5eb1f1fae2db219f23ab3ec32e15f680bcbe
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 21:10:56 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -2,53 +2,13 @@
 
 mkdir -p artifacts
 
-# Properly iterate over elements in the array
-if [[ "${#TEST_MODULES[@]}" -gt 1 ]]; then
-  echo "TEST_MODULES is treated as an array"
-  for MODULE in "${TEST_MODULES[@]}"; do
-    echo "1Processing TestNG results for \"$MODULE\""
-    MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
-
-    # Display MODULE_PATH for debugging purposes
-    echo "1MODULE_PATH: $MODULE_PATH"
-
-    if [ -d "$MODULE_PATH" ]; then
-      mkdir -p "artifacts/$MODULE"
-      cp -r "$MODULE_PATH" "artifacts/$MODULE/"
-    else
-      echo "No TestNG results found for \"$MODULE\""
-    fi
-  done
-else
-  echo "TEST_MODULES is being treated as a single string"
-  # If treated as a single string, iterate over space-separated values
-  for MODULE in $TEST_MODULES; do
-    echo "2Processing TestNG results for \"$MODULE\""
-    MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
-
-    # Display MODULE_PATH for debugging purposes
-    echo "2MODULE_PATH: $MODULE_PATH"
-
-    if [ -d "$MODULE_PATH" ]; then
-      mkdir -p "artifacts/$MODULE"
-      cp -r "$MODULE_PATH" "artifacts/$MODULE/"
-    else
-      echo "No TestNG results found for \"$MODULE\""
-    fi
-  done
-fi
-
-
-#
-#mkdir -p artifacts
-#
-#for MODULE in $TEST_MODULES; do
-#  echo "Processing TestNG results for \"$MODULE\""
-#  MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
-#  if [ -d "$MODULE_PATH" ]; then
-#    mkdir -p "artifacts/$MODULE"
-#    cp -r "$MODULE_PATH" "artifacts/$MODULE/"
-#  else
-#    echo "No TestNG results found for \"$MODULE\""
-#  fi
-#done
+for MODULE in $TEST_MODULES; do
+  echo "Processing TestNG results for \"$MODULE\""
+  MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
+  if [ -d "$MODULE_PATH" ]; then
+    mkdir -p "artifacts/$MODULE"
+    cp -r "$MODULE_PATH" "artifacts/$MODULE/"
+  else
+    echo "No TestNG results found for \"$MODULE\""
+  fi
+done
