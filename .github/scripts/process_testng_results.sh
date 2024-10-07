363634f1f1fcf77fbbf91b2240401ce1240574d2
Kamil Nowocin <knowocin@sii.pl>
2024-10-07 20:56:58 +0200

    test

--- .github/scripts/process_testng_results.sh
+++ .github/scripts/process_testng_results.sh
@@ -2,13 +2,11 @@
 
 mkdir -p artifacts
 
-for MODULE in "${TEST_MODULES[@]}"; do
+mkdir -p artifacts
+
+for MODULE in $TEST_MODULES; do
   echo "Processing TestNG results for \"$MODULE\""
-  MODULE_PATH="$GITHUB_WORKSPACE/$MODULE/build/reports/tests/test"
-
-  # Display MODULE_PATH for debugging purposes
-  echo "MODULE_PATH: $MODULE_PATH"
-
+  MODULE_PATH="/home/runner/work/test/test/$MODULE/build/reports/tests/test"
   if [ -d "$MODULE_PATH" ]; then
     mkdir -p "artifacts/$MODULE"
     cp -r "$MODULE_PATH" "artifacts/$MODULE/"
