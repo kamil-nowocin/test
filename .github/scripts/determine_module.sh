587a244b31b5506f6a1b5b95f96174afd4f1c68d
Kamil Nowocin <knowocin@sii.pl>
2024-10-08 09:46:24 +0200

    test

--- .github/scripts/determine_module.sh
+++ .github/scripts/determine_module.sh
@@ -2,12 +2,14 @@
 
 if [[ "$GITHUB_EVENT_NAME" == "schedule" ]]; then
   CURRENT_TIME=$((10#$(date -u +"%H%M")))
-  if [[ "$CURRENT_TIME" -ge 730 && "$CURRENT_TIME" -lt 735 ]]; then
+  if [[ "$CURRENT_TIME" -ge 755 && "$CURRENT_TIME" -lt 815 ]]; then
     BACKEND_MODULE="WFE_Websters"
     GRADLE_TASK="$BACKEND_MODULE:test"
-  elif [[ "$CURRENT_TIME" -ge 750 && "$CURRENT_TIME" -lt 808 ]]; then
+    TEST_MODULES=("$BACKEND_MODULE")
+  elif [[ "$CURRENT_TIME" -ge 820 && "$CURRENT_TIME" -lt 840 ]]; then
     BACKEND_MODULE="WFE_Klasters"
     GRADLE_TASK="$BACKEND_MODULE:test"
+    TEST_MODULES=("$BACKEND_MODULE")
   else
     echo "No matching time found for $CURRENT_TIME! Exiting..."
     exit 1
@@ -22,16 +24,14 @@ elif [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]]; then
     TEST_MODULES=("$BACKEND_MODULE")
   fi
 else
-  echo "This workflow was triggered by unsupported event! Exiting..."
+  echo "This workflow was triggered by an unsupported event! Exiting..."
   exit 1
 fi
 
-TEST_MODULES_STRING="${TEST_MODULES[*]}"
 {
   echo "BACKEND_MODULE=$BACKEND_MODULE"
   echo "GRADLE_TASK=$GRADLE_TASK"
-  echo "TEST_MODULES=$TEST_MODULES_STRING"
+  echo "TEST_MODULES=${TEST_MODULES[*]}"
 } >> "$GITHUB_ENV"
 
 echo "Selected backend module: $BACKEND_MODULE"
-echo "Serialized TEST_MODULES: $TEST_MODULES_STRING"
