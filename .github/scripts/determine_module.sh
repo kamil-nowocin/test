7da77fb30334d9f3fe6916150010ae25581f71e2
Kamil Nowocin <knowocin@sii.pl>
2024-10-08 10:22:51 +0200

    test

--- .github/scripts/determine_module.sh
+++ .github/scripts/determine_module.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
+initialize_module_settings() {
+  BACKEND_MODULE="$1"
+  GRADLE_TASK="$BACKEND_MODULE:test"
+  TEST_MODULES=("$BACKEND_MODULE")
+}
+
 if [[ "$GITHUB_EVENT_NAME" == "schedule" ]]; then
   CURRENT_TIME=$((10#$(date -u +"%H%M")))
-  if [[ "$CURRENT_TIME" -ge 755 && "$CURRENT_TIME" -lt 815 ]]; then
-    BACKEND_MODULE="WFE_Websters"
-    GRADLE_TASK="$BACKEND_MODULE:test"
-    TEST_MODULES=("$BACKEND_MODULE")
-  elif [[ "$CURRENT_TIME" -ge 820 && "$CURRENT_TIME" -lt 840 ]]; then
-    BACKEND_MODULE="WFE_Klasters"
-    GRADLE_TASK="$BACKEND_MODULE:test"
-    TEST_MODULES=("$BACKEND_MODULE")
-  else
-    echo "No matching time found for $CURRENT_TIME! Exiting..."
-    exit 1
-  fi
+  case $CURRENT_TIME in
+    825|840) initialize_module_settings "WFE_Websters" ;;
+    845|900) initialize_module_settings "WFE_Klasters" ;;
+    *)
+  echo "No matching time found for $CURRENT_TIME! Exiting..." exit 1 ;;
+  esac
 elif [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]]; then
   BACKEND_MODULE="$GITHUB_INPUTS_MODULE"
   if [[ "$BACKEND_MODULE" == "WFE_All" ]]; then
     GRADLE_TASK="test"
     TEST_MODULES=("WFE_Websters" "WFE_Klasters")
   else
-    GRADLE_TASK="$BACKEND_MODULE:test"
-    TEST_MODULES=("$BACKEND_MODULE")
+    initialize_module_settings "$BACKEND_MODULE"
   fi
 else
   echo "This workflow was triggered by an unsupported event! Exiting..."
