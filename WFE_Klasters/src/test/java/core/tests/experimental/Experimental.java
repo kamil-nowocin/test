a97c8bc01d0b7f1e00234e34d3c68a884eb3e169
Kamil Nowocin <knowocin@sii.pl>
2024-10-08 12:30:22 +0200

    test

--- WFE_Klasters/src/test/java/core/tests/experimental/Experimental.java
+++ WFE_Klasters/src/test/java/core/tests/experimental/Experimental.java
@@ -14,7 +14,7 @@ public class Experimental {
   @Test(testName = "FIRST EXPERIMENTAL TEST - NAME", description = "FIRST EXPERIMENTAL TEST - DESC")
   public void experimentalTest2() {
     System.out.println("KLASTERS SECOND EXPERIMENTAL TEST!");
-    Assert.fail("ON PURPOSE!");
+    Assert.fail("ON PURPOSE!")
   }
 
   @Test(testName = "FIRST EXPERIMENTAL TEST - NAME", description = "FIRST EXPERIMENTAL TEST - DESC")
