535e3bb91d5d9604714baea3d0489371cebc3b0a
Kamil Nowocin <knowocin@sii.pl>
2024-10-30 20:46:21 +0100

    test

--- WFE_Klasters/src/test/java/core/tests/experimental/Experimental.java
+++ WFE_Klasters/src/test/java/core/tests/experimental/Experimental.java
@@ -39,7 +39,11 @@ public class Experimental {
   public void setup() {
     WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
-    options.addArguments("--incognito");
+    options.addArguments("--headless"); // Uruchamia przeglądarkę w trybie bezgłowym
+    options.addArguments("--disable-gpu"); // Wyłącza GPU (zalecane dla CI)
+    options.addArguments("--no-sandbox"); // Wyłącza sandboxing
+    options.addArguments("--disable-dev-shm-usage"); // Zmniejsza zużycie pamięci współdzielonej
+    options.addArguments("--incognito"); // Włącza tryb incognito
     driver = new ChromeDriver(options);
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
